a773616ccb274de23d67f056b80b28c935299610
XianpingMa <xianpingma@link.cuhk.edu.cn>
2024-11-26 16:46:45 +0800

    Update utils.py

--- MFNet/utils.py
+++ MFNet/utils.py
@@ -16,7 +16,8 @@ WINDOW_SIZE = (256, 256) # Patch size
 STRIDE = 32 # Stride for testing
 IN_CHANNELS = 3 # Number of input channels (e.g. RGB)
 FOLDER = "/media/lscsc/nas/xianping/ISPRS_dataset/" # Replace with your "/path/to/the/ISPRS/dataset/folder/"
-BATCH_SIZE = 10 # Number of samples in a mini-batch
+BATCH_SIZE = 10 
+# BATCH_SIZE = 4 # For backbone ViT-Huge
 
 LABELS = ["roads", "buildings", "low veg.", "trees", "cars", "clutter"] # Label names
 N_CLASSES = len(LABELS) # Number of classes
